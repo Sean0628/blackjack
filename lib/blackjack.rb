a0b34053e03490fb2561073f04c069ae6818824e
Sho Ito <i.sho.0628@gmail.com>
2018-05-07 17:57:24 +0900

    refactor several methods to make it clear

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -20,40 +20,13 @@ class BlackJack
     puts_blank_line
     init_app
     puts "あなたの合計得点は#{@player.score}です"
-
-    loop do
-    puts 'カードを引きますか？(yes/no)'
-    input = gets.chomp.downcase
-      if input == 'yes'
-        @player.draw(@deck)
-        puts "あなたの合計得点は#{@player.score}です"
-        player_burst?(@player.score)
-      elsif input == 'no'
-        break
-      else
-        puts "不正な値です"
-      end
-    end
-
-    while @dealer.score <= 17
-      @dealer.draw(@deck)
-      puts "ディーラーの合計得点は#{@dealer.score}です"
-      puts_blank_line
-      dealer_burst?(@dealer.score)
-    end
-
-    # 両者がカードを引き終えた時の処理
-    puts "あなたの合計得点は#{@player.score}です"
-    puts "ディーラーの合計得点は#{@dealer.score}です"
-    if @player.score > @dealer.score
-      puts 'あなたの勝ちです'
-    elsif @player.score < @dealer.score
-      puts 'あなたの負けです'
-    else
-      puts '引き分けです'
-    end
-
+    player_draws
+    dealer_draws
+    return_result
+    puts '---------- Thank you. Bye! ----------'
   end
+
+  private
 
   def init_app
     2.times { @player.draw(@deck) }
@@ -76,6 +49,43 @@ class BlackJack
     if score > 21
       puts "カードの合計が21を超えました\nあなたの勝ちです"
       exit
+    end
+  end
+
+  def player_draws
+    loop do
+    puts 'カードを引きますか？(yes/no)'
+    input = gets.chomp.downcase
+      if input == 'yes'
+        @player.draw(@deck)
+        puts "あなたの合計得点は#{@player.score}です"
+        player_burst?(@player.score)
+      elsif input == 'no'
+        break
+      else
+        puts "不正な値です"
+      end
+    end
+  end
+
+  def dealer_draws
+    while @dealer.score <= 17
+      @dealer.draw(@deck)
+      puts "ディーラーの合計得点は#{@dealer.score}です"
+      puts_blank_line
+      dealer_burst?(@dealer.score)
+    end
+  end
+
+  def return_result
+    puts "あなたの合計得点は#{@player.score}です"
+    puts "ディーラーの合計得点は#{@dealer.score}です"
+    if @player.score > @dealer.score
+      puts 'あなたの勝ちです'
+    elsif @player.score < @dealer.score
+      puts 'あなたの負けです'
+    else
+      puts '引き分けです'
     end
   end
 end
